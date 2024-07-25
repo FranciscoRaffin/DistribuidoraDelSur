75d629bc787fa95a6fc6ef294a87fe0ce1736600
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-07-25 17:08:01 -0300

    set entry point 0.3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  entry: './dist/index.html'
+  entry: 'dist/index.html'
 })
