cd6d7f0af477a0042ae075c5263bc914a66be3dd
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-07-25 17:37:27 -0300

    entry 0.4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  entry: '/dist/index.html'
+  build: {
+    outDir: 'dist'
+  }
 })
