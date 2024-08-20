6c4359a762735c384bea107a735c1dab0eeed15a
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-08-19 21:48:19 -0300

    re-styling

--- src/assets/elements/Search.jsx
+++ src/assets/elements/Search.jsx
@@ -3,7 +3,7 @@ import './../styles/Search.css';
 export function Search() {
     return (
         <div className="search">
-            <input placeholder="🔍 Buscar por ID" type="text" />
+            <input placeholder="🔍 Buscar por nombre o ID " type="text" />
             <button type="submit">Buscar</button>
         </div>
     );
